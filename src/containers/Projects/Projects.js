6e582cd9116a8c2e63c155cdaa891d0dffe14c13
JerryZhengPro <jerryzhengpro@gmail.com>
2022-04-10 20:59:43 -0700

    Add new project

--- src/containers/Projects/Projects.js
+++ src/containers/Projects/Projects.js
@@ -18,6 +18,12 @@ class Projects extends Component {
             profile and stats. I also implemented a Django backend for authentication and access to a database. This project came solely from my 
             imagination and creativity outside of school. While making it, I had to reflect on and revise my code numerous times for best practices 
             and optimization purposes.`; 
+
+        const descriptionC = (
+            `This is a database of anime I created using C++. The user can list, search for, delete, 
+            and add anime. The only library other than STL I used is ncurses, which I used for the 
+            GUI.`
+        ); 
             
         return (
             <>
@@ -39,6 +45,12 @@ class Projects extends Component {
                     image={spin} 
                     title="Spin"
                     date="July 2020 - Present"
+                    credits="Jerry Zheng"
+                />
+                <Project
+                    description={descriptionC}
+                    title="Anime Database"
+                    date="April 2022 - Present"
                     credits="Jerry Zheng"
                 />
                 <div style={{height: "200px"}}/>
