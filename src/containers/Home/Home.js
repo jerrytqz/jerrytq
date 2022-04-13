19de6d57dc0d434d78640b68f09d43eac8def375
JerryZhengPro <jerryzhengpro@gmail.com>
2022-04-12 21:26:57 -0700

    Reorder imports

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,10 +1,10 @@
 import React, {Component} from 'react'; 
 
 import classes from './Home.module.css'; 
-import Extracurriculars from '../../components/Extracurriculars/Extracurriculars';
-import Overview from '../../components/Overview/Overview';
 import ProfileMain from '../../components/ProfileMain/ProfileMain'; 
+import Overview from '../../components/Overview/Overview';
 import Skills from '../../components/Skills/Skills'; 
+import Extracurriculars from '../../components/Extracurriculars/Extracurriculars';
 import Achievements from '../../components/Achievements/Achievements'; 
 import Certifications from '../../components/Achievements/Certifications';
 
