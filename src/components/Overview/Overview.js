9f954ff0abea93a7e93c9b3f57917d586022467d
JerryZhengPro <jerryzhengpro@gmail.com>
2022-04-19 22:55:26 -0700

    Update description for about me

--- src/components/Overview/Overview.js
+++ src/components/Overview/Overview.js
@@ -5,15 +5,17 @@ import OverviewSection from './OverviewSection/OverviewSection';
 const Overview = () => {
     const titleA = "About Me"; 
     const bodyA = 
-        `I am currently a high school student in Grade 12 at Fraser Heights Secondary School. For as far back as I can remember,
-        I have loved math and science, especially computer science and physics. I am highly interested in STEM and plan on working
-        in the field down the road.`;
+        `I am currently a high school student in Grade 12 at Fraser Heights Secondary School. Next 
+        year, I will be studying software engineering at the University of Waterloo. For as far back 
+        as I can remember, I have loved STEM, especially math, computer science, and 
+        physics. In the future, I plan on working as a software engineer.`;
 
     const titleB = "Hobbies"; 
     const bodyB = 
-        `Learning more about my favourite subjects has always filled me with joy and excitement, and as such, I can often be found
-        studying math (especially for upcoming math competitions) and science (such as by working on my newest coding project). 
-        Otherwise, I'm usually playing badminton with my friends, or if it's winter, skiing on a mountain.`;
+        `Learning more about my favourite subjects has always filled me with joy and excitement, 
+        and as such, I can often be found studying math (especially for upcoming math competitions) 
+        and science (such as by working on my newest coding project). Otherwise, I'm usually playing 
+        badminton with my friends, or if it's winter, skiing on a mountain.`;
 
     return (
         <section>
