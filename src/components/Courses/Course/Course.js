a65da87585912ae9a40cf9179e801edd89b73356
jerrytqz <jerryzhengpro@gmail.com>
2022-12-30 14:48:03 -0800

    Fix bubble background flicker on iOS

--- src/components/Courses/Course/Course.js
+++ src/components/Courses/Course/Course.js
@@ -5,7 +5,7 @@ import classes from './Course.module.css';
 const Course = (props) => {
     return (
         <tr style={props.style}>
-            <td className={[classes.Data, classes.Min768TableCell].join(' ')}>{props.term.name} ({props.term.period})</td>
+            <td className={[classes.Data, classes.Min768TableCell].join(' ')}>{props.term.period}</td>
             <td className={classes.Data}>{props.name}</td>
             <td className={classes.Data}>{props.description}</td>
             <td className={classes.Data}>{props.grade || 'TBD'}</td>
