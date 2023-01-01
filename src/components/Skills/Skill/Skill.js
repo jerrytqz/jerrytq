35027667ba8fdef61debf8e4eed7e10249d9850d
jerrytqz <jerryzhengpro@gmail.com>
2022-12-31 21:42:37 -0800

    Tweak skills area appearance

--- src/components/Skills/Skill/Skill.js
+++ src/components/Skills/Skill/Skill.js
@@ -6,7 +6,7 @@ import classes from './Skill.module.css';
 const Skill = (props) => {
     return (
         <div className={classes.Container}>
-            <Technology imageUrl={props.imageUrl} alt={props.imageAlt} style={{width: '72px', height: '72px'}}/>
+            <Technology imageUrl={props.imageUrl} alt={props.imageAlt}/>
             <p className={classes.Name}>{props.name}</p>
         </div>
     );
--- src/components/Technology/Technology.js
+++ src/components/Technology/Technology.js
@@ -4,7 +4,7 @@ import classes from './Technology.module.css';
 
 const Technology = (props) => {
     return (
-        <img style={props.style} src={props.imageUrl} alt={props.imageAlt} className={classes.Image} draggable={false}/>
+        <img src={props.imageUrl} alt={props.imageAlt} className={classes.Image} draggable={false}/>
     );
 }
 
