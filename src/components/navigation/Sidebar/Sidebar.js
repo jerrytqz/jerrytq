62a0f805f34d99b193be9d562fd50c0a0c475cfb
jerrytqz <jerryzhengpro@gmail.com>
2024-12-30 00:48:59 -0800

    Wrap Sidebar with FocusTrap

--- src/components/navigation/Sidebar/Sidebar.js
+++ src/components/navigation/Sidebar/Sidebar.js
@@ -1,3 +1,4 @@
+import { FocusTrap } from 'focus-trap-react';
 import React from 'react';
 
 import Backdrop from '../../../shared/userInterfaces/Backdrop/Backdrop';
@@ -5,26 +6,36 @@ import MenuButton from '../../../shared/userInterfaces/buttons/MenuButton/MenuBu
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './Sidebar.module.css';
 
-const Sidebar = (props) => (
-  <>
-    <div
-      className={[classes.Container, props.show ? classes.Show : '']
-        .join(' ')
-        .trim()}
-      inert={props.show ? undefined : ''} // Workaround until upgrade to React 19
-    >
-      <header className={classes.Header}>
-        <MenuButton onClick={props.onSidebarClose} ariaLabel="Close sidebar" />
-      </header>
-      <nav className={classes.NavItems}>
-        <NavigationItems />
-      </nav>
-      <footer className={classes.Footer}>
-        <p>© 2022 Jerry Zheng</p>
-      </footer>
-    </div>
-    <Backdrop show={props.show} onClick={props.onSidebarClose} />
-  </>
-);
+const Sidebar = (props) => {
+  return (
+    <>
+      <FocusTrap
+        active={props.show}
+        focusTrapOptions={{ clickOutsideDeactivates: true }}
+      >
+        <div
+          className={[classes.Container, props.show ? classes.Show : '']
+            .join(' ')
+            .trim()}
+          inert={props.show ? undefined : ''} // Temporary workaround until upgrade to React 19
+        >
+          <header className={classes.Header}>
+            <MenuButton
+              onClick={props.onSidebarClose}
+              ariaLabel="Close sidebar"
+            />
+          </header>
+          <nav className={classes.NavItems}>
+            <NavigationItems />
+          </nav>
+          <footer className={classes.Footer}>
+            <p>© 2022 Jerry Zheng</p>
+          </footer>
+        </div>
+      </FocusTrap>
+      <Backdrop show={props.show} onClick={props.onSidebarClose} />
+    </>
+  );
+};
 
 export default Sidebar;
