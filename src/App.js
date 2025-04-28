03571f25cab1c8c8c8ccb7b91e50f0dbf6d2624d
jerrytqz <jerryzhengpro@gmail.com>
2025-04-27 20:52:04 -0700

    Adjust contact hero padding

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ const App = () => {
           </Routes>
         </ErrorBoundary>
       </Layout>
-      <Analytics />
+      <Analytics debug={false} />
       <ScrollRestoration />
     </>
   );
