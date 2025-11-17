284e89972d5c2b2ba4f42ae719bd0efc8ec56877
jerrytqz <jerryzhengpro@gmail.com>
2025-11-17 00:36:32 -0500

    Update overview

--- src/components/Overview/Overview.js
+++ src/components/Overview/Overview.js
@@ -6,7 +6,7 @@ import OverviewPart from './OverviewPart/OverviewPart';
 const OVERVIEW = [
   {
     title: 'About Me',
-    body: `I am currently a 3rd-year Software Engineering student at the University of Waterloo. 
+    body: `I am a 4th-year Software Engineering student at the University of Waterloo. 
         I taught myself to code in middle school with Java and quickly developed a passion for 
         creating software. Since then, I have interned at several companies in various software 
         development roles. In my free time, I enjoy working on programming projects that 
